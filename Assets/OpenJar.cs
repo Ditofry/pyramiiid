99740396bbafb466d2e1718b22a5ac72eca47377
vicki388 <vickieverts1@gmail.com>
2015-11-18 23:06:20 -0700

    added explorer functionality

--- Assets/OpenJar.cs
+++ Assets/OpenJar.cs
@@ -4,20 +4,42 @@ using System.Collections;
 public class OpenJar : MonoBehaviour {
     public float moveSpeed = 10f;
     public float turnSpeed = 50f;
+    //RaycastHit myhit = new RaycastHit();
+    //Ray myray = new Ray();
+
 	void Start () {
       
 	
 	}
 	
 	void Update () {
-        if(Input.GetKey(KeyCode.UpArrow))
+
+        /*myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(myray, out myhit, 1000.0f) && Input.GetMouseButtonDown(0))
+            StartCoroutine("moveJar");*/
+        if (Input.GetKey(KeyCode.I))
             transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.M))
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.K))
             transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.J))
             transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
+         
+                    
+       
       
     }
+    /*void moveJar()
+    {
+        if (Input.GetKey(KeyCode.I))
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.M))
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.K))
+            transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.J))
+            transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
+
+    }*/
 }
