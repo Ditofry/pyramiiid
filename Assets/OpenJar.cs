77516d8bbcebea5c8cc5e8fd7e2e98835f91be4c
vicki388 <vickieverts1@gmail.com>
2015-12-06 15:53:39 -0700

    added text

--- Assets/OpenJar.cs
+++ Assets/OpenJar.cs
@@ -17,19 +17,39 @@ public class OpenJar : MonoBehaviour {
         /*myray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(myray, out myhit, 1000.0f) && Input.GetMouseButtonDown(0))
             StartCoroutine("moveJar");*/
-        if (Input.GetKey(KeyCode.I))
+        /*if (Input.GetKey(KeyCode.I))
             transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.M))
             transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.K))
             transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.J))
+            transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);*/
+        if (Input.GetButtonDown("OpenUp"))
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("OpenDown"))
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("TwistRight"))
+            transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("TwistLeft"))
             transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
          
                     
        
       
     }
+    /*void OnMouseDown()
+    {
+        if (Input.GetButtonDown("OpenUp"))
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("OpenDown"))
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("TwistRight"))
+            transform.Rotate(Vector3.right, -turnSpeed * Time.deltaTime);
+        if (Input.GetButtonDown("TwistLeft"))
+            transform.Rotate(Vector3.left, -turnSpeed * Time.deltaTime);
+
+    }*/
     /*void moveJar()
     {
         if (Input.GetKey(KeyCode.I))
