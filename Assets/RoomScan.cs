f2f0b19470c761cafda0e24e59a449f132dd6ed2
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2015-12-08 09:54:42 -0700

    added a bunch of assetts...again

---
+++ Assets/RoomScan.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RoomScan : MonoBehaviour {
+
+	// Attached AudioFiles
+	public AudioClip subterraneanChamber;
+	public AudioClip descendingPassage;
+	public AudioClip ascendingPassage;
+	public AudioClip queensRoom;
+	public AudioClip kingsRoom;
+	// Model for our scanner effect
+	public GameObject scannerRadiation;
+	bool scanned;
+
+	// Cache GetComponent for brevity later on as well as performance
+	private AudioSource source;
+	
+	void Awake () {	source = GetComponent<AudioSource>(); }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scanned = CrossPlatformInputManager.GetButtonDown("Scan");
+		if (scanned)
+			Instantiate (scannerRadiation);
+	}
+	
+}
---
+++ Assets/ScanRadiation.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScanRadiation : MonoBehaviour {
+
+	// Should it be the concern of the player script to determine spawn location?
+	private Transform explorerTransform;
+	private Transform scanBubble;
+	Bounds bounds;
+
+	void Awake() {
+		// ¯\_(ツ)_/¯
+		scanBubble = this.transform;
+		explorerTransform = GameObject.FindGameObjectWithTag ("Player").transform;
+		// We want the sphere to expand in front of explorer
+		scanBubble.up = explorerTransform.up;
+		scanBubble.position = explorerTransform.position;
+	}
+
+	void Grow(){
+		scanBubble.localScale += new Vector3(1.5F, 1.5F, 1.5F);
+	}
+
+	void Dissapate(){
+		Destroy (gameObject);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (scanBubble.localScale.magnitude < 100) {
+			Grow ();
+		} else Dissapate();
+	}
+}
